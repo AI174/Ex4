79115162cdcd5de652ec95ff07693eb2983dd232
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-03-12 17:32:29 +0200

    Trunk - documented

--- src/pepse/world/trees/Trunk.java
+++ src/pepse/world/trees/Trunk.java
@@ -3,30 +3,43 @@ package pepse.world.trees;
 import danogl.GameObject;
 import danogl.components.GameObjectPhysics;
 import danogl.gui.rendering.RectangleRenderable;
-import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
 import pepse.util.ColorSupplier;
+import pepse.world.AvatarObserver;
+import pepse.world.Block;
+import java.awt.Color;
 
-import java.awt.*;
+/**
+ * Represents a trunk object in the game world.
+ * @author adan.ir1, hayanat2002
+ * @see GameObject
+ * @see AvatarObserver
+ */
 
-public class Trunk extends GameObject {
+public class Trunk extends GameObject implements AvatarObserver {
 
     private static final Color TRUNK_COLOR = new Color(100, 50, 20);
     private static final String TRUNK_TAG = "Trunk";
-    private static final int TRUNK_WIDTH = 30; // DON'T CHANGE -> changed from 10 to 30
+    private static final int TRUNK_WIDTH = Block.SIZE;
 
     /**
-     * Construct a new GameObject instance.
-     *
-     * @param DownLeftCorner Position of the object, in window coordinates (pixels).
-     *                      Note that (0,0) is the top-left corner of the window.
+     * Constructs a new Trunk object with the given position and height.
+     * @param downLeftCorner The bottom-left corner position of the trunk.
+     * @param height The height of the trunk.
      */
-    public Trunk(Vector2 DownLeftCorner,int height) {
-        super(DownLeftCorner.add(new Vector2(0, -height)), new Vector2(TRUNK_WIDTH, height),
+    public Trunk(Vector2 downLeftCorner,int height) {
+        super(downLeftCorner.add(new Vector2(0, -height)), new Vector2(TRUNK_WIDTH, height),
                 new RectangleRenderable(ColorSupplier.approximateColor(TRUNK_COLOR)));
         physics().preventIntersectionsFromDirection(Vector2.ZERO);
         physics().setMass(GameObjectPhysics.IMMOVABLE_MASS);
         this.setTag(TRUNK_TAG);
     }
 
+    /**
+     * Updates the trunk appearance when the avatar jumps.
+     */
+    @Override
+    public void updateWhenAvatarJumps() {
+        this.renderer().setRenderable(new RectangleRenderable(ColorSupplier.approximateColor(TRUNK_COLOR)));
+    }
 }
