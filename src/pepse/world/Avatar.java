34413b6ca7b041709725ec05509bb97bfd65257c
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-03-12 16:00:05 +0200

    Avatar - documented

--- src/pepse/world/Avatar.java
+++ src/pepse/world/Avatar.java
@@ -6,46 +6,71 @@ import danogl.gui.UserInputListener;
 import danogl.gui.rendering.AnimationRenderable;
 import danogl.gui.rendering.Renderable;
 import danogl.util.Vector2;
-
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
+/**
+ * Represents the avatar character in the game world.
+ * @author adan.ir1, hayanat2002
+ * @see GameObject
+ * @see AnimationRenderable
+ * @see KeyEvent
+ */
 public class Avatar extends GameObject {
-    public static final int AVATAR_HEIGHT = 60; //don't change
-    public static final int AVATAR_WIDTH = 30 ; //don't change
-    private static final float VELOCITY_X = 300; //don't change
-    private static final float VELOCITY_Y = -450; //don't change
-    private static final float GRAVITY = 500; //don't change
-    private static final float CHANGING_TIME = .4f; //don't change
-    private float energy = 100;
+    /**
+     * The height of the avatar.
+     */
+    public static final int AVATAR_HEIGHT = 60;
+    /**
+     * The width of the avatar.
+     */
+    public static final int AVATAR_WIDTH = 40 ;
+    private static final float VELOCITY_X = 300;
+    private static final float VELOCITY_Y = -450;
+    private static final float GRAVITY = 500;
+    private static final float CHANGING_TIME = .4f;
+    private static final String AVATAR_TAG = "avatar";
+    private static final String AVATAR_IMAGE = "assets/idle_0.png";
+    private static final String AVATAR_ITH_IMAGE = "assets/idle_";
+    private static final String PNG = ".png";
+    private static final String AVATAR_JUMP_IMAGE = "assets/jump_";
+    private static final String AVATAR_RAN_IMAGE = "assets/run_";
     private static Renderable imageRenderable;
     private final UserInputListener inputListener;
     private final ImageReader imageReader;
-    private static final String AVATAR_TAG = "avatar";
-    private boolean inTheAir = false;
+    private final ArrayList<AvatarObserver> observerList = new ArrayList<>();
+    private float energy = 100;
     private AnimationRenderable notMovingAnimation;
     private AnimationRenderable upDownAnimation;
     private AnimationRenderable sideAnimation;
 
     /**
-     * hhh
-     * @param pos down right
-     * @param inputListener input
-     * @param imageReader imageReader
+     * Constructs an avatar object with the specified position, input listener, and image reader.
+     * @param pos The position of the avatar.
+     * @param inputListener The input listener for handling user input.
+     * @param imageReader The image reader for reading avatar images.
      */
     public Avatar(Vector2 pos, UserInputListener inputListener, ImageReader imageReader){
         super(pos.add(new Vector2(-AVATAR_WIDTH, -AVATAR_HEIGHT)),
-                new Vector2(AVATAR_WIDTH, AVATAR_HEIGHT), imageRenderable); // must change wrong parameters
+                new Vector2(AVATAR_WIDTH, AVATAR_HEIGHT), imageRenderable);
         this.inputListener = inputListener;
         this.imageReader = imageReader;
         transform().setAccelerationY(GRAVITY);
         this.setTag(AVATAR_TAG);
         initializeAnimation();
-    }
 
+    }
+    /**
+     * Creates an avatar object with the specified position, input listener, and image reader.
+     * @param downRightCorner The bottom-right corner position of the avatar.
+     * @param inputListener The input listener for handling user input.
+     * @param imageReader The image reader for reading avatar images.
+     * @return An avatar object.
+     */
     public static Avatar create(Vector2 downRightCorner,UserInputListener inputListener,
                                 ImageReader imageReader) {
 
-        Avatar.imageRenderable = imageReader.readImage("assets/idle_0.png", true);
+        Avatar.imageRenderable = imageReader.readImage(AVATAR_IMAGE, true);
         Avatar avatar = new Avatar(downRightCorner, inputListener, imageReader);
         avatar.physics().preventIntersectionsFromDirection(Vector2.ZERO);
 
@@ -53,14 +78,38 @@ public class Avatar extends GameObject {
 
     }
 
+    /**
+     * Updates the avatar's state based on the elapsed time.
+     * @param deltaTime The time elapsed, in seconds, since the last frame.
+     * Used for determining new positions/velocities.
+     */
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
         move();
         createAnimation();
     }
-
-
+    /**
+     * Adds energy to the avatar.
+     * @param n The amount of energy to add.
+     */
+    public void addEnergy(float n){
+        energy = Math.min(100, energy + n);
+    }
+    /**
+     * Gets the energy
+     * @return the avatar's energy
+     */
+    public float getEnergy(){
+        return energy;
+    }
+    /**
+     * Registers an observer to observe the avatar's actions.
+     * @param observer The observer to register.
+     */
+    public void registerObserver(AvatarObserver observer){
+        observerList.add(observer);
+    }
     private void move() {
         float xVel = 0;
         if(inputListener.isKeyPressed(KeyEvent.VK_LEFT) && energy >= .5f ) {
@@ -80,46 +129,34 @@ public class Avatar extends GameObject {
         if(inputListener.isKeyPressed(KeyEvent.VK_SPACE) && getVelocity().y() == 0 && energy >= 10){
             transform().setVelocityY(VELOCITY_Y);
             energy -= 10f;
+            updateObservers();
         }
 
         if(getVelocity().isZero()){
             energy = Math.min(100f, energy + 1);
         }
     }
-
-
-    public void addEnergy(float n){
-        energy = Math.min(100, energy + n);
-    }
-    public float getEnergy(){
-        return energy;
-    }
-
-
-    /**
-     * tomorowwwwwwwwwwwwwwwwww job
-     */
     private void initializeAnimation(){
         Renderable[] notMovingArr = new Renderable[4];
         for (int i = 0; i < 4; i++) {
-            notMovingArr[i] = imageReader.readImage("assets/idle_"+i+".png", true);
+            notMovingArr[i] = imageReader.readImage(AVATAR_ITH_IMAGE +i+ PNG,
+                    true);
         }
 
         Renderable[] upDownArr = new Renderable[4];
         for (int i = 0; i < 4; i++) {
-            upDownArr[i] = imageReader.readImage("assets/jump_"+i+".png", true);
+            upDownArr[i] = imageReader.readImage(AVATAR_JUMP_IMAGE +i+PNG, true);
         }
 
         Renderable[] sideArr= new Renderable[6];
         for (int i = 0; i < 6; i++) {
-            sideArr[i] = imageReader.readImage("assets/run_"+i+".png", true);
+            sideArr[i] = imageReader.readImage(AVATAR_RAN_IMAGE +i+PNG, true);
         }
 
         notMovingAnimation = new AnimationRenderable(notMovingArr, CHANGING_TIME);
         upDownAnimation = new AnimationRenderable(upDownArr, CHANGING_TIME);
         sideAnimation = new AnimationRenderable(sideArr, CHANGING_TIME);
     }
-
     private void createAnimation(){
         if(getVelocity().isZero()){
             renderer().setRenderable(notMovingAnimation);
@@ -131,6 +168,13 @@ public class Avatar extends GameObject {
         }
         else if(getVelocity().x() != 0){
             renderer().setRenderable(sideAnimation);
+        }
+    }
+    private void updateObservers(){
+        for (AvatarObserver avatarObserver : observerList) {
+            if (avatarObserver != null) {
+                avatarObserver.updateWhenAvatarJumps();
+            }
         }
     }
 }
