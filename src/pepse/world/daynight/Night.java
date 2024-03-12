2675723270f15634e4eda551cec8dc5e2b2cddbd
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-03-12 15:18:42 +0200

    Night - documented

--- src/pepse/world/daynight/Night.java
+++ src/pepse/world/daynight/Night.java
@@ -5,20 +5,26 @@ import danogl.components.CoordinateSpace;
 import danogl.components.Transition;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
-
-import java.awt.*;
-
+import java.awt.Color;
+/**
+ * Represents the night cycle in the game world.
+ * @author adan.ir1, hayanat2002
+ * @see GameObject
+ * @see Transition
+ */
 public class Night {
 
     private static final String NIGHT_TAG = "night";
     private static final float MIDNIGHT_OPACITY = 0.5f;
     private static final float MIDDAY_OPACITY = 0f;
-    public static final int TRANSFER_FACTOR = 2;
-
+    private static final int TRANSFER_FACTOR = 2;
+    /**
+     * Creates a night GameObject with the specified window dimensions and cycle length.
+     * @param windowDimensions The dimensions of the game window.
+     * @param cycleLength The length of the day-night cycle.
+     * @return The GameObject representing the night.
+     */
     public static GameObject create(Vector2 windowDimensions, float cycleLength){
-        // I'm not sure about cycleLength/2, seems more reasonable to me :
-        // cycleLength is the day ,lets say 24 hours, from 12pm till 12 am it should go from 0 to 0.5 , then
-        // from 12am to 12 pm it should go from 0.5 to 0 -> transfer time is 12 hours --> cycleLength / 2
         GameObject night = new GameObject(Vector2.ZERO,windowDimensions,new RectangleRenderable(Color.BLACK));
         night.setCoordinateSpace(CoordinateSpace.CAMERA_COORDINATES);
         night.setTag(NIGHT_TAG);
