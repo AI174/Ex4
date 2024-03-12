53eefbf2ebb1b16646d6a7f00de70040fe1ecffb
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-03-12 15:42:14 +0200

    SunHalo - documented

--- src/pepse/world/daynight/SunHalo.java
+++ src/pepse/world/daynight/SunHalo.java
@@ -3,16 +3,22 @@ package pepse.world.daynight;
 import danogl.GameObject;
 import danogl.components.CoordinateSpace;
 import danogl.gui.rendering.OvalRenderable;
-
-
-import java.awt.*;
-
+import java.awt.Color;
+/**
+ * Represents the sun halo in the game world.
+ * @author adan.ir1, hayanat2002
+ * @see GameObject
+ */
 public class SunHalo {
 
     private static final String SUN_HALO_TAG = "sunHalo";
     private static final Color HALO_COLOR = new Color(255, 255, 0, 20);
-    private static final float SUN_HALO_FACTOR = 1.5f;
-
+    private static final float SUN_HALO_FACTOR = 1.7f;
+    /**
+     * Creates a sun halo GameObject according to the given sun.
+     * @param sun The sun to create its halo.
+     * @return The GameObject representing the sun halo.
+     */
     public static GameObject create(GameObject sun){
         GameObject sunHalo = new GameObject(sun.getTopLeftCorner(),sun.getDimensions().mult(SUN_HALO_FACTOR),
                 new OvalRenderable(HALO_COLOR));
