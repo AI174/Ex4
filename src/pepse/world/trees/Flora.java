ca7463b0779ba636a95220894375a7673f6a4534
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-03-12 17:37:52 +0200

    Flora - documented

--- src/pepse/world/trees/Flora.java
+++ src/pepse/world/trees/Flora.java
@@ -3,50 +3,61 @@ package pepse.world.trees;
 import danogl.GameObject;
 import danogl.util.Vector2;
 import pepse.world.Block;
-
-import java.awt.*;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Random;
 import java.util.function.Function;
 
+/**
+ * Represents the flora in the game world, including trees, leaves, and fruits.
+ * @author adan.ir1, hayanat2002
+ * @see Trunk
+ * @see Leaf
+ * @see Fruit
+ */
 public class Flora{
-
-
     private static final int TRUNK_HEIGHT_UPPER_BOUND = 180;
     private static final int TRUNK_HEIGHT_LOWER_BOUND = 100;
+    private static final int LEAF_SIZE = Block.SIZE;
+    private static final int LEAVES_RANGE_ROW = 3;
+    private static final int LEAVES_RANGE_COL = 2;
+    private static final float ADD_TREE_BOUND = .9f;
+    private static final float ADD_LEAF_BOUND = .2f;
+    private static final float ADD_FRUIT_BOUND = .9f;
     private final Random random =new Random();
     private final Function<Float, Float> groundHeightAt;
-    private final Vector2 windowDimensions;
-//    private final int seed;
 
-
-    public Flora(Function<Float, Float> groundHeightAt,Vector2 windowDimensions) {
-
+    /**
+     * Constructs a Flora object with the specified ground height function.
+     * @param groundHeightAt A function that returns the ground height at a given x.
+     */
+    public Flora(Function<Float, Float> groundHeightAt) {
         this.groundHeightAt = groundHeightAt;
-        this.windowDimensions = windowDimensions;
     }
 
-//    public
-
+    /**
+     * Creates flora objects within the specified range.
+     * Trees, leaves, and fruits are generated based on random factors.
+     * @param minX The minimum x.
+     * @param maxX The maximum x.
+     * @return A list of GameObjects representing the flora objects created.
+     */
     public ArrayList<GameObject> createInRange(int minX, int maxX){
         ArrayList<GameObject> trees = new ArrayList<>();
         minX = (int) Math.floor((double) minX / Block.SIZE) * Block.SIZE;
         maxX = (int) Math.floor((double) maxX / Block.SIZE) * Block.SIZE;
-
         for (int i = minX; i <= maxX; i+= Block.SIZE) {
-
+            if(i == 0){ // because the avatar is there
+                continue;
+            }
             float randomFloat = random.nextFloat();
-            // they want the trees in different heights
             int trunkHeight = random.nextInt(TRUNK_HEIGHT_UPPER_BOUND - TRUNK_HEIGHT_LOWER_BOUND) +
                     TRUNK_HEIGHT_LOWER_BOUND;
-            // we want it to be rounded to the proper height
             trunkHeight = (int)Math.floor((float)trunkHeight/ Block.SIZE) * Block.SIZE;
 
-            if(randomFloat > 0.9f){
+            if(randomFloat > ADD_TREE_BOUND){
                 trees.add(buildTree((float) i,trunkHeight));
-                trees.addAll(buildTreeLeafs(i,trunkHeight)); // add all the leafs for the curr tree
+                trees.addAll(buildTreeUpperComponents(i,trunkHeight));
             }
         }
         return trees;
@@ -58,23 +69,26 @@ public class Flora{
         return trunk;
     }
 
-    private List<GameObject> buildTreeLeafs(int x, int trunkHeight) {
-        ArrayList<GameObject> leafs= new ArrayList<>();
-        for (int i = x-90; i <=x+90 ; i+=30) {
-            for (int j = trunkHeight- 120;j<= trunkHeight + 120;j+=30){
+    private List<GameObject> buildTreeUpperComponents(int x, int trunkHeight) {
+        ArrayList<GameObject> treeUpperComponents= new ArrayList<>();
+        for (int i = (x-LEAF_SIZE*LEAVES_RANGE_ROW); i <=(x+LEAF_SIZE*LEAVES_RANGE_ROW) ; i+=LEAF_SIZE) {
+            for (int j = (trunkHeight- LEAF_SIZE*LEAVES_RANGE_COL);
+                 j<= (trunkHeight + LEAF_SIZE*LEAVES_RANGE_COL);j+=LEAF_SIZE){
+
                 float randomFloat = random.nextFloat();
-                if(randomFloat > 0.9f){
-                    Leaf leaf = new Leaf(new Vector2(i,j),Vector2.ONES.mult(30f));
-                    leafs.add(leaf);
+                if(randomFloat > ADD_LEAF_BOUND){
+                    Leaf leaf = new Leaf(new Vector2(i, groundHeightAt.apply((float)i) - j),
+                            Vector2.ONES.mult(LEAF_SIZE));
+                    treeUpperComponents.add(leaf);
+                }
+
+                float fruitRandomFloat = random.nextFloat();
+                if (fruitRandomFloat > ADD_FRUIT_BOUND){
+                    Fruit fruit = new Fruit(new Vector2(i, groundHeightAt.apply((float)i) - j));
+                    treeUpperComponents.add(fruit);
                 }
             }
-
-
         }
-        return leafs;
+        return treeUpperComponents;
     }
-
-
-
-
 }
