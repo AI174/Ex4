6bfdb2961426835e6e7ada8496b510485f6fc525
adan.ir1 <adan.irshead@mail.huji.ac.il>
2024-03-12 14:58:31 +0200

    sky - documented

--- src/pepse/world/Sky.java
+++ src/pepse/world/Sky.java
@@ -4,13 +4,20 @@ import danogl.GameObject;
 import danogl.components.CoordinateSpace;
 import danogl.gui.rendering.RectangleRenderable;
 import danogl.util.Vector2;
-
-import java.awt.*;
-
+import java.awt.Color;
+/**
+ * Represents the sky in the game world.
+ * @author adan.ir1, hayanat2002
+ * @see GameObject
+ */
 public class Sky {
     private static final Color BASIC_SKY_COLOR = Color.decode("#80C6E5");
     private static final String SKY_TAG = "sky";
-
+    /**
+     * Creates a sky GameObject with the specified window dimensions.
+     * @param windowDimensions The dimensions of the game window.
+     * @return The GameObject representing the sky.
+     */
     public static GameObject create(Vector2 windowDimensions){
         GameObject sky = new GameObject(Vector2.ZERO,windowDimensions,
                 new RectangleRenderable(BASIC_SKY_COLOR));
